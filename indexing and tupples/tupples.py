cf7426ce5f73a4d2037f5657c498bceede08d7dd
Frank Entsie <frank.entsie301@gmail.com>
2023-07-17 10:04:05 +0900

    tupples and string mutation with replace

--- indexing and tupples/tupples.py
+++ indexing and tupples/tupples.py
@@ -1,5 +1,12 @@
 filenames = ["1. raw data.txt","2. reports.txt","3. presentations.txt"]
+sample_tupples = ("one", "two")
 
-for filename in filenames:
-    fixed_filename = filename.replace(".","-") 
-    print(fixed_filename)
+
+# for filename in filenames:
+#     fixed_filename = filename.replace(".","-", 1) 
+#     print(fixed_filename)
+
+for sample_tupple in sample_tupples:
+    print(sample_tupple)
+
+print(sample_tupples.index("one"))
